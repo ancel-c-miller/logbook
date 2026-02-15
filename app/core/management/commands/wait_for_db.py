a60217269d9efe6e741a6d22f97ae283fbc20b2f
Ancel Miller <ancel.c.miller@gmail.com>
2026-02-14 20:46:39 -0700

    race condition

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,22 @@
+"""
+Django management command to wait for the database to be ready.
+"""
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    """Django command to wait for the database to be ready."""
+
+    def handle(self, *args, **options):
+        """Handle the command."""
+        self.stdout.write("Waiting for database...")
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections["default"]
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting 1 second...")
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
